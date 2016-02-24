46e8c7454eb016177e4d15a721a93d169f6e8d13
Andrés Maneiro <andres.maneiro@gmail.com>
2016-02-24 19:07:03 +0100

    better defaults

--- models/Fonte.js
+++ models/Fonte.js
@@ -4,7 +4,9 @@ Backbone.SIXHIARA.Fonte = Backbone.Model.extend({
   defaults: {
     'tipo_agua':  '',
     'tipo_fonte': '',
-    'c_requerid': '',
+    'c_requerid': null,
+    'c_max':      null,
+    'c_real':     null,
     'comentario': '',
   },
 
