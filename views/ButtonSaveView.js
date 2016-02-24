66c3e5ee846abbaa0f63de255039f99515e54289
Francisco Puga <fpuga@icarto.es>
2016-02-24 08:47:16 +0100

    refs #1088. Don't serialize licenses if they are empty

--- views/ButtonSaveView.js
+++ views/ButtonSaveView.js
@@ -9,6 +9,21 @@ Backbone.SIXHIARA.ButtonSaveView = Backbone.View.extend({
   // },
 
   save: function(){
+
+    // FIXME. If Licencia is Emtpy the object should no be serialized
+    var lics = this.model.get('licencias');
+    var long = 0;
+    if (_.isArray(lics)) {
+        long = lics.length
+    } else {
+        long = lics.models.length;
+    }
+
+    for (var i = 0; i < long; i++) {
+      if (! lics.at(0).get('lic_nro') ) lics.remove(lics.at(0));
+    }
+
+
     c = new Backbone.SIXHIARA.ExploracaoCollection();
     c.create(this.model)
     // make API configurable through config.js
