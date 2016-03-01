b83cd94944f7eab2dff8094848c484fc7fa0d966
Francisco Puga <fpuga@icarto.es>
2016-03-01 20:39:12 +0100

    Exploracao create nested models and collections on get requests

--- models/Exploracao.js
+++ models/Exploracao.js
@@ -24,6 +24,7 @@ Backbone.SIXHIARA.Exploracao = Backbone.GeoJson.Feature.extend({
     'c_estimado': null,
     'actividade': null,
     'area':       null,
+    'geometry':   null,
     'utente':     new Backbone.SIXHIARA.Utente(),
     'licencias':  new Backbone.SIXHIARA.LicenciaCollection(),
     'fontes':     new Backbone.SIXHIARA.FonteCollection(),
@@ -31,6 +32,33 @@ Backbone.SIXHIARA.Exploracao = Backbone.GeoJson.Feature.extend({
     'consumo': 'C', // FIXME. Está en las fixtures
     'estado':  'L', // FIXME. Está en las fixtures
     // 'pagos':   'P', // FIXME. Está en las fixtures. En create tiene que ser un booleano
+  },
+
+  parse: function(response){
+    response = Backbone.GeoJson.Feature.prototype.parse.apply(this, arguments);
+
+    if (_.has(response, 'utente')) {
+      response.utente = new Backbone.SIXHIARA.Utente(response.utente)
+    }
+
+    if (_.has(response, 'licencias')) {
+      response.licencias = new Backbone.SIXHIARA.LicenciaCollection(response.licencias)
+    }
+
+    if (_.has(response, 'fontes')) {
+      response.fontes = new Backbone.SIXHIARA.FonteCollection(response.fontes)
+    }
+
+    return response;
+  },
+
+  toJSON: function() {
+    var json =  _.clone(this.attributes);
+    json.geometry = this.get('geometry').toJSON();
+    json.utente = this.get('utente').toJSON();
+    json.licencias = this.get('licencias').toJSON();
+    json.fontes = this.get('fontes').toJSON();
+    return json;
   },
 
   validate: function(attrs, options){
@@ -56,13 +84,13 @@ Backbone.SIXHIARA.Exploracao = Backbone.GeoJson.Feature.extend({
     var containsAttrs = _.isEqual(properties, values);
     var containsUtente = true;
     if (where.attributes.utente) {
-      containsUtente = (this.get('utente').nome === where.attributes.utente);
+      containsUtente = (this.get('utente').get('nome') === where.attributes.utente);
     }
     var containsLic = true;
     if(where.attributes.lic_tipo || where.attributes.estado){
       containsLic = false;
       var whereLic = _.pick(where.values(), 'lic_tipo', 'estado');
-      var lics = new Backbone.SIXHIARA.LicenciaCollection(this.get('licencias')).where(whereLic);
+      var lics = this.get('licencias').where(whereLic);
       if (lics.length > 0) {
         containsLic = true;
       }
