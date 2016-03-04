57d33da8d6781224e9155f61bdb9037efdaa1714
Andrés Maneiro <amaneiro@icarto.es>
2016-03-04 12:24:46 +0100

    Add contador formatting

--- views/TableRowShowView.js
+++ views/TableRowShowView.js
@@ -3,7 +3,7 @@ Backbone.SIXHIARA.TableRowShowView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td><i class="fa fa-caret-down"></i></td><td><%- tipo_agua %></td><td><%- tipo_fonte %></td><td><%- lat_lon %></td><td><% print(formatter().formatNumber(c_soli)) %></td><td><% print(formatter().formatNumber(c_max)) %></td><td><% print(formatter().formatNumber(c_real)) %></td><td><% print(formatter().formatDate(d_dado)) %></td><td><%- contador %></td><td><%- metodo_est %></td><td><%- comentario %></td>'),
+  template: _.template('<td><i class="fa fa-caret-down"></i></td><td><%- tipo_agua %></td><td><%- tipo_fonte %></td><td><%- lat_lon %></td><td><% print(formatter().formatNumber(c_soli)) %></td><td><% print(formatter().formatNumber(c_max)) %></td><td><% print(formatter().formatNumber(c_real)) %></td><td><% print(formatter().formatDate(d_dado)) %></td><td><% print(formatter().formatBoolean(contador)) %></td><td><%- metodo_est %></td><td><%- comentario %></td>'),
 
   events:{
     'click .close': 'modelDestroy'
