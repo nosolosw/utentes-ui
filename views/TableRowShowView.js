b7cfe4f2b444b684aaab4b706991c383bc784a43
Andrés Maneiro <amaneiro@icarto.es>
2016-03-04 12:38:33 +0100

    show: style edit button and add buttons for 'Nova fonte'

--- views/TableRowShowView.js
+++ views/TableRowShowView.js
@@ -3,7 +3,7 @@ Backbone.SIXHIARA.TableRowShowView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td><i class="fa fa-caret-down"></i></td><td><%- tipo_agua %></td><td><%- tipo_fonte %></td><td><%- lat_lon %></td><td><% print(formatter().formatNumber(c_soli)) %></td><td><% print(formatter().formatNumber(c_max)) %></td><td><% print(formatter().formatNumber(c_real)) %></td><td><% print(formatter().formatDate(d_dado)) %></td><td><% print(formatter().formatBoolean(contador)) %></td><td><%- metodo_est %></td><td><%- comentario %></td>'),
+  template: _.template('<td><%- tipo_agua %></td><td><%- tipo_fonte %></td><td><%- lat_lon %></td><td><% print(formatter().formatNumber(c_soli)) %></td><td><% print(formatter().formatNumber(c_max)) %></td><td><% print(formatter().formatNumber(c_real)) %></td><td><% print(formatter().formatDate(d_dado)) %></td><td><% print(formatter().formatBoolean(contador)) %></td><td><%- metodo_est %></td><td><%- comentario %></td><td><button type="button" class="btn btn-default btn-xs">Editar</button> <button type="button" class="btn btn-default btn-xs">Eliminar</button></td>'),
 
   events:{
     'click .close': 'modelDestroy'
