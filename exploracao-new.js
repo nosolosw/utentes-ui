add9fc7aa40536f8bc107dc94dca2e33be4c8e5d
Andrés Maneiro <amaneiro@icarto.es>
2016-03-08 12:40:20 +0100

    new: fix it

--- exploracao-new.js
+++ exploracao-new.js
@@ -4,91 +4,22 @@ $(document).ready(function() {
   });
 });
 
-// var domains = DOMAINS_REPO;
-
 var domains = new Backbone.UILib.DomainCollection({url: '/api/domains'});
-
 domains.fetch({
   success: function(collection, response, options) {
-
-    var provincias      = domains.byCategory('provincia');
-    var distritos       = domains.byCategory('distrito');
-    var postos          = domains.byCategory('posto');
-    var bacias          = domains.byCategory('bacia');
-    var subacias        = domains.byCategory('subacia');
-    var estadosLicencia = domains.byCategory('licencia_estado');
-    var actividades     = domains.byCategory('actividade');
-    var fonteTipos      = domains.byCategory('fonte_tipo');
-
-    // block localización
-    new Backbone.UILib.SelectView({
-      el: $('#loc_provin'),
-      collection: provincias
-    }).render();
-
-    var selectDistritos = new Backbone.UILib.SelectView({
-      el: $('#loc_distri'),
-      collection: [],
-    }).render();
-    selectDistritos.listenTo(exploracao, 'change:loc_provin', function(model, value, options){
-      this.update(distritos.where({'parent': model.get('loc_provin')}));
-    });
-
-    var selectPostos = new Backbone.UILib.SelectView({
-      el: $('#loc_posto'),
-      collection: [],
-    }).render();
-    selectPostos.listenTo(exploracao, 'change:loc_distri', function(model, value, options){
-      this.update(postos.where({'parent': model.get('loc_distri')}));
-    });
-
-    new Backbone.UILib.SelectView({
-      el: $('#loc_bacia'),
-      collection: bacias
-    }).render();
-
-    var selectSubacias = new Backbone.UILib.SelectView({
-      el: $('#loc_subaci'),
-      collection: [],
-    }).render();
-    selectSubacias.listenTo(exploracao, 'change:loc_bacia', function(model, value, options){
-      this.update(subacias.where({'parent': model.get('loc_bacia')}));
-    });
-
-    // block licencias
-    new Backbone.UILib.SelectView({
-      el: $('.estado-superficial'),
-      collection: estadosLicencia
-    }).render();
-
-    new Backbone.UILib.SelectView({
-      el: $('.estado-subterranea'),
-      collection: estadosLicencia
-    }).render();
-
-    new Backbone.UILib.SelectView({
-      el: $('#actividade'),
-      collection: actividades
-    }).render();
-
-    // fontes
-    new Backbone.UILib.SelectView({
-      el: $('#fonteSubModal #fonte_tipo'),
-      collection: fonteTipos.byParent('Subterrânea')
-    }).render();
-    new Backbone.UILib.SelectView({
-      el: $('#fonteSupModal #fonte_tipo'),
-      collection: fonteTipos.byParent('Superficial')
-    }).render();
-
+    fillComponentsWithDomains();
   }
 });
 
+var utentes = new Backbone.SIXHIARA.UtenteCollection();
+utentes.fetch({
+  success: function() {
+    fillSelectUtente();
+  }
+});
 
-
+// model to save
 var exploracao = new Backbone.SIXHIARA.Exploracao();
-
-// add skeleton licencias
 var licenciaSubterranea = new Backbone.SIXHIARA.Licencia({
   'lic_tipo': 'Subterrânea'
 });
@@ -98,56 +29,67 @@ var licenciaSuperficial = new Backbone.SIXHIARA.Licencia({
 exploracao.get('licencias').add(licenciaSuperficial);
 exploracao.get('licencias').add(licenciaSubterranea);
 
+// save action
 new Backbone.SIXHIARA.ButtonSaveView({
   el: $('#save-button'),
   model: exploracao
 }).render();
 
-// block info
+// page info
 new Backbone.UILib.WidgetsView({
   el: $('#info'),
   model: exploracao
 }).render();
 
-
-// block utente
-// var utentes = UTENTES_REPO;
-var utentes = new Backbone.SIXHIARA.UtenteCollection();
-utentes.fetch({
-  success: function() {
-    new Backbone.SIXHIARA.SelectUtenteView({
-      el: $('#utente'),
-      model: utentes
-    }).render();
-  }
-});
-
+// page utente
+function fillSelectUtente(){
+  new Backbone.SIXHIARA.SelectUtenteView({
+    el: $('#utente'),
+    model: utentes
+  }).render();
+}
 new Backbone.UILib.WidgetsView({
   el: $('#utente'),
   model: exploracao.get('utente')
 }).render();
 
-
-
-// TODO: look for a better way to organize this
+// page actividade
 new Backbone.UILib.WidgetsView({
   el: $('#actividade-explotacion'),
   model: exploracao
 }).render();
 
-
-// block licencias
+// page licencias & fontes: licencias
+new Backbone.UILib.WidgetsView({
+  el: $('#licencia-superficial'),
+  model: licenciaSuperficial
+}).render();
 
 new Backbone.UILib.WidgetsView({
   el: $('#licencia-subterranea'),
   model: licenciaSubterranea
 }).render();
 
-new Backbone.UILib.WidgetsView({
-  el: $('#licencia-superficial'),
-  model: licenciaSuperficial
-}).render();
+$('#fonte-subterranea').on('click', function(e){
+  e.preventDefault();
+  $('#fonteSubModal').modal('toggle');
+});
+new Backbone.SIXHIARA.ModalFonteView({
+  el: $('#fonteSubModal'),
+  collection: exploracao.get('fontes')
+});
 
+// page licencias & fontes: fontes modal
+$('#fonte-superficial').on('click', function(e){
+  e.preventDefault();
+  $('#fonteSupModal').modal('toggle');
+});
+new Backbone.SIXHIARA.ModalFonteView({
+  el: $('#fonteSupModal'),
+  collection: exploracao.get('fontes')
+});
+
+// page licencias & fontes: fontes table
 var tableFontesView = new Backbone.SIXHIARA.TableFontesView({
   el: $('#fontes'),
   collection: exploracao.get('fontes')
@@ -159,22 +101,100 @@ tableFontesView.listenTo(exploracao.get('fontes'), 'destroy', function(model, co
   this.update(exploracao.get('fontes'));
 });
 
+function fillComponentsWithDomains(){
 
-$('#fonte-subterranea').on('click', function(e){
-  e.preventDefault();
-  $('#fonteSubModal').modal('toggle');
-});
-new Backbone.SIXHIARA.ModalFonteView({
-  el: $('#fonteSubModal'),
-  collection: exploracao.get('fontes')
-});
+  var provincias      = domains.byCategory('provincia');
+  var distritos       = domains.byCategory('distrito');
+  var postos          = domains.byCategory('posto');
+  var bacias          = domains.byCategory('bacia');
+  var subacias        = domains.byCategory('subacia');
+  var estadosLicencia = domains.byCategory('licencia_estado');
+  var actividades     = domains.byCategory('actividade');
+  var fonteTipos      = domains.byCategory('fonte_tipo');
 
+  // page info: localizacao
+  new Backbone.UILib.SelectView({
+    el: $('#info #loc_provin'),
+    collection: provincias
+  }).render();
 
-$('#fonte-superficial').on('click', function(e){
-  e.preventDefault();
-  $('#fonteSupModal').modal('toggle');
-});
-new Backbone.SIXHIARA.ModalFonteView({
-  el: $('#fonteSupModal'),
-  collection: exploracao.get('fontes')
-});
+  var selectDistritos = new Backbone.UILib.SelectView({
+    el: $('#info #loc_distri'),
+    collection: [],
+  }).render();
+  selectDistritos.listenTo(exploracao, 'change:loc_provin', function(model, value, options){
+    this.update(distritos.where({'parent': model.get('loc_provin')}));
+  });
+
+  var selectPostos = new Backbone.UILib.SelectView({
+    el: $('#info #loc_posto'),
+    collection: [],
+  }).render();
+  selectPostos.listenTo(exploracao, 'change:loc_distri', function(model, value, options){
+    this.update(postos.where({'parent': model.get('loc_distri')}));
+  });
+
+  new Backbone.UILib.SelectView({
+    el: $('#info #loc_bacia'),
+    collection: bacias
+  }).render();
+
+  var selectSubacias = new Backbone.UILib.SelectView({
+    el: $('#info #loc_subaci'),
+    collection: [],
+  }).render();
+  selectSubacias.listenTo(exploracao, 'change:loc_bacia', function(model, value, options){
+    this.update(subacias.where({'parent': model.get('loc_bacia')}));
+  });
+
+  // page utente: localizacion
+  new Backbone.UILib.SelectView({
+    el: $('#utente #loc_provin'),
+    collection: provincias
+  }).render();
+
+  var distritosUtente = new Backbone.UILib.SelectView({
+    el: $('#utente #loc_distri'),
+    collection: [],
+  }).render();
+  distritosUtente.listenTo(exploracao.get('utente'), 'change:loc_provin', function(model, value, options){
+    this.update(distritos.where({'parent': model.get('loc_provin')}));
+  });
+
+  var postosUtente = new Backbone.UILib.SelectView({
+    el: $('#utente #loc_posto'),
+    collection: [],
+  }).render();
+  postosUtente.listenTo(exploracao.get('utente'), 'change:loc_distri', function(model, value, options){
+    this.update(postos.where({'parent': model.get('loc_distri')}));
+  });
+
+  // page actividade: actividades
+  new Backbone.UILib.SelectView({
+    el: $('#actividade'),
+    collection: actividades
+  }).render();
+
+  // page licencias & fontes: estados
+  new Backbone.UILib.SelectView({
+    el: $('.estado-superficial'),
+    collection: estadosLicencia
+  }).render();
+
+  new Backbone.UILib.SelectView({
+    el: $('.estado-subterranea'),
+    collection: estadosLicencia
+  }).render();
+
+  // page licencias & fontes: tipos fonte
+  new Backbone.UILib.SelectView({
+    el: $('#fonteSubModal #fonte_tipo'),
+    collection: fonteTipos.byParent('Subterrânea')
+  }).render();
+
+  new Backbone.UILib.SelectView({
+    el: $('#fonteSupModal #fonte_tipo'),
+    collection: fonteTipos.byParent('Superficial')
+  }).render();
+
+}
--- views/ModalFonteView.js
+++ views/ModalFonteView.js
@@ -9,7 +9,7 @@ Backbone.SIXHIARA.ModalFonteView = Backbone.View.extend({
     this.collection.add(new Backbone.SIXHIARA.Fonte({
       'tipo_agua':  this.$('#tipo_agua').val(),
       'tipo_fonte': this.$('#fonte_tipo').val(),
-      'c_soli':     this.$('#c_soli').val(),
+      'c_soli':     formatter().unformatNumber(this.$('#c_soli').val()),
       'observacio': this.$('#observacio').val(),
     }));
     // close on click or let it open to enter several fontes?
