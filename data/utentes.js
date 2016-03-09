ab78ea36524966cbee9d6f4f5bbe9dd4871d6c76
Andrés Maneiro <amaneiro@icarto.es>
2016-03-09 13:50:42 +0100

    delete unused resources

--- data/utentes.js
+++
@@ -1,4 +0,0 @@
-var UTENTES_REPO = new Backbone.SIXHIARA.UtenteCollection([
-  {'nome': 'Anadarco Mozambique', 'nuit': '456', 'reg_comerc': '', 'reg_zona': ''},
-  {'nome': 'iCarto', 'nuit': '123', 'reg_comerc': '', 'reg_zona': ''},
-]);
