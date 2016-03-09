f9b8603a1d42f5673416060198e3c558ad6e2620
Francisco Puga <fpuga@icarto.es>
2016-03-09 10:13:42 +0100

    Agregates localizacion and reg columns

--- utentes.js
+++ utentes.js
@@ -14,6 +14,31 @@ var formatValue = function(k, v, rowData) {
     v = '<button type="button" class="btn btn-default btn-xs edit">Editar</button>';
     v += '<br><br>';
     v += '<button type="button" class="btn btn-default btn-xs delete">Eliminar</button>';
+  } else if (k === 'registro'){
+    var com = rowData.get('reg_comerc'),
+        zon = rowData.get('reg_zona');
+
+    if (!com && !zon) {
+      v = '';
+    } else {
+        v = (com || '-') + ' / ' + (zon || '-');
+    }
+
+  } else if (k === 'localizacion') {
+    var prov = rowData.get('loc_provin'),
+        dist = rowData.get('loc_distri'),
+        post = rowData.get('loc_posto'),
+        nucl = rowData.get('loc_nucleo');
+
+  if (!prov && !dist) {
+    v = '';
+  } else {
+    v = (prov || '-') + ' / ' + (dist || '-');
+  }
+
+  if (post) v += '<br><span class="title">Posto:</span> ' + post;
+  if (nucl) v += '<br><span class="title">Núcleo:</span> ' + nucl;
+
   } else if (_.isEmpty(v)) {
     v = '';
   } else if (v === false) {
@@ -29,17 +54,21 @@ var formatValue = function(k, v, rowData) {
 var tableUtentes = new Backbone.SIXHIARA.TableUtentes({
   collection: utentes,
   el: $('#the_utentes_table'),
-  columnNames: ['nome', 'entidade', 'reg_comerc', 'reg_zona', 'loc_provin', 'loc_distri', 'loc_posto', 'loc_nucleo', 'exploracaos', 'observacio', 'actions'],
+  columnNames: ['nome', 'entidade', 'registro', 'localizacion', 'exploracaos', 'observacio', 'actions'],
   // 'actions' column is used to render the buttons
   // 'nuit', Is show with 'nome' and not it its own column
+  // 'reg_comerc', 'reg_zona' are shown as 'registro'
+  // 'loc_provin', 'loc_distri', 'loc_posto', 'loc_nucleo' are shown as 'localizacion'
   columnsWithOutTitle: ['actions'],
   columnTitles: {
     'id':          'ID',
     'nome':        'Nome',
     'entidade':    'Entidade',
     'nuit':        'Nuit',
+    'registro':    'Reg. (Comercial / Zona)',
     'reg_comerc':  'Reg. Comercial',
     'reg_zona':    'Reg. Zona',
+    'localizacion':'Localización',
     'loc_provin':  'Provincia',
     'loc_distri':  'Distrito',
     'loc_posto':   'Posto',
