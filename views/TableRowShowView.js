ef746c4749f5515002cff5a15c610564154f25bd
Andrés Maneiro <amaneiro@icarto.es>
2016-03-15 13:20:33 +0100

    show: allow fontes to be removed

--- views/TableRowShowView.js
+++ views/TableRowShowView.js
@@ -3,10 +3,14 @@ Backbone.SIXHIARA.TableRowShowView = Backbone.View.extend({
 
   tagName: 'tr',
 
-  template: _.template('<td><%- tipo_agua %></td><td><%- tipo_fonte %></td><td><%- lat_lon %></td><td><% print(formatter().formatNumber(c_soli)) %></td><td><% print(formatter().formatNumber(c_max)) %></td><td><% print(formatter().formatNumber(c_real)) %></td><td><% print(formatter().formatDate(d_dado)) %></td><td><% print(formatter().formatBoolean(contador)) %></td><td><%- metodo_est %></td><td><%- observacio %></td><td><button type="button" class="btn btn-default btn-xs">Editar</button> <button type="button" class="btn btn-default btn-xs">Eliminar</button></td>'),
+  template: _.template('<td><%- tipo_agua %></td><td><%- tipo_fonte %></td><td><%- lat_lon %></td><td><% print(formatter().formatNumber(c_soli)) %></td><td><% print(formatter().formatNumber(c_max)) %></td><td><% print(formatter().formatNumber(c_real)) %></td><td><% print(formatter().formatDate(d_dado)) %></td><td><% print(formatter().formatBoolean(contador)) %></td><td><%- metodo_est %></td><td><%- observacio %></td><td><button type="button" class="btn btn-default btn-xs">Editar</button> <button type="button" class="delete-fonte btn btn-default btn-xs">Eliminar</button></td>'),
 
   events:{
-    'click .close': 'modelDestroy'
+    'click .delete-fonte': 'modelDestroy'
+  },
+
+  initialize: function(){
+    this.model.on('remove', this.unrender, this);
   },
 
   render: function(){
@@ -15,8 +19,12 @@ Backbone.SIXHIARA.TableRowShowView = Backbone.View.extend({
     return this;
   },
 
+  unrender: function(){
+    this.$el.remove();
+  },
+
   modelDestroy: function(){
-    this.model.destroy();
+    this.model.collection.remove(this.model);
   }
 
 });
