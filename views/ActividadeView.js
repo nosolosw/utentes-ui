89274ff55cc05d58cbbe405e1697c7799fda8b73
Andrés Maneiro <amaneiro@icarto.es>
2016-03-16 13:30:13 +0100

    ActividadeView returns itself

--- views/ActividadeView.js
+++ views/ActividadeView.js
@@ -3,6 +3,8 @@ Backbone.SIXHIARA.ActividadeView = Backbone.View.extend({
 
   render: function(){
     this.$('#actividade').text(this.model.get('actividade').get('tipo') || 'Actividade non declarada');
+
+    return this;
   }
 
 });
