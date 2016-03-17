2a32d39334b6a202ddea49532bc77d7211693897
Andrés Maneiro <amaneiro@icarto.es>
2016-03-17 15:00:53 +0100

    modularize localizacao block

--- views/show/ExploracaoShowView.js
+++ views/show/ExploracaoShowView.js
@@ -91,7 +91,8 @@ Backbone.SIXHIARA.ExploracaoShowView = Backbone.View.extend({
 
     var locBlockView = new Backbone.SIXHIARA.LocBlockView({
       el: $('#loc-info'),
-      model: exploracao
+      model: exploracao,
+      domains: this.domains,
     }).render();
     this.subViews.push(locBlockView);
 
@@ -212,54 +213,9 @@ Backbone.SIXHIARA.ExploracaoShowView = Backbone.View.extend({
   fillComponentsWithDomains: function(){
     var exploracao = this.model;
     var domains = this.domains;
-    var provincias  = domains.byCategory('provincia');
-    var distritos   = domains.byCategory('distrito');
-    var postos      = domains.byCategory('posto');
-    var bacias      = domains.byCategory('bacia');
-    var subacias    = domains.byCategory('subacia');
     var actividades = domains.byCategory('actividade');
     var estadosLic  = domains.byCategory('licencia_estado');
     var fonteTipos  = domains.byCategory('fonte_tipo');
-
-    // modal loc
-    new Backbone.UILib.SelectView({
-      el: $('#editLocModal #loc_provin'),
-      collection: provincias
-    }).render();
-
-    var selectDistritos = new Backbone.UILib.SelectView({
-      el: $('#editLocModal #loc_distri'),
-      collection: []
-    }).render();
-    selectDistritos.listenTo(exploracao, 'change:loc_provin', function(model, value, options){
-      this.update(distritos.where({'parent': model.get('loc_provin')}));
-    });
-
-    var selectPostos = new Backbone.UILib.SelectView({
-      el: $('#editLocModal #loc_posto'),
-      collection: []
-    }).render();
-    selectPostos.listenTo(exploracao, 'change:loc_distri', function(model, value, options){
-      this.update(postos.where({'parent': model.get('loc_distri')}));
-    });
-
-    new Backbone.UILib.SelectView({
-      el: $('#editLocModal #loc_bacia'),
-      collection: bacias
-    }).render();
-
-    var selectSubacias = new Backbone.UILib.SelectView({
-      el: $('#editLocModal #loc_subaci'),
-      collection: [],
-    }).render();
-    selectSubacias.listenTo(exploracao, 'change:loc_bacia', function(model, value, options){
-      this.update(subacias.where({'parent': model.get('loc_bacia')}));
-    });
-
-    new Backbone.UILib.WidgetsView({
-      el: $('#editLocModal'),
-      model: exploracao
-    }).render();
 
     // modal actividade: actividades
     new Backbone.UILib.SelectView({
--- views/show/LocBlockView.js
+++ views/show/LocBlockView.js
@@ -1,36 +1,90 @@
 Backbone.SIXHIARA = Backbone.SIXHIARA || {};
 Backbone.SIXHIARA.LocBlockView = Backbone.View.extend({
 
-  initialize: function () {
+  // TODO:
+  // - modal: selects do not update model
+
+  initialize: function (options) {
     this.subViews = [];
 
     var exploracao = this.model;
-    exploracao.on('render', function () {
-      this.render();
-    }, this);
+    this.model.on('render', this.render, this);
+    options.domains.on('sync', this.renderModal, this);
 
-    // block loc-info & its modal
     var locView = new Backbone.UILib.WidgetsView({
       el: $('#loc-info'),
       model: exploracao
     });
     this.subViews.push(locView);
 
-    $('#editLoc').on('click', function(e){
-      e.preventDefault();
-      $('#editLocModal').modal('toggle');
-    });
   },
 
   render: function () {
-    _.invoke(this.subviews, 'render');
+    _.invoke(this.subViews, 'render');
 
     return this;
   },
 
+  renderModal: function (collection, response, options) {
+    // connect edit button
+    $('#editLoc').on('click', function(e){
+      e.preventDefault();
+      $('#editLocModal').modal('toggle');
+    });
+
+    var exploracao = this.model;
+    var domains = collection;
+    var provincias  = domains.byCategory('provincia');
+    var distritos   = domains.byCategory('distrito');
+    var postos      = domains.byCategory('posto');
+    var bacias      = domains.byCategory('bacia');
+    var subacias    = domains.byCategory('subacia');
+
+    // modal loc
+    new Backbone.UILib.SelectView({
+      el: $('#editLocModal #loc_provin'),
+      collection: provincias
+    }).render();
+
+    var selectDistritos = new Backbone.UILib.SelectView({
+      el: $('#editLocModal #loc_distri'),
+      collection: []
+    }).render();
+    selectDistritos.listenTo(exploracao, 'change:loc_provin', function(model, value, options){
+      this.update(distritos.where({'parent': model.get('loc_provin')}));
+    });
+
+    var selectPostos = new Backbone.UILib.SelectView({
+      el: $('#editLocModal #loc_posto'),
+      collection: []
+    }).render();
+    selectPostos.listenTo(exploracao, 'change:loc_distri', function(model, value, options){
+      this.update(postos.where({'parent': model.get('loc_distri')}));
+    });
+
+    new Backbone.UILib.SelectView({
+      el: $('#editLocModal #loc_bacia'),
+      collection: bacias
+    }).render();
+
+    var selectSubacias = new Backbone.UILib.SelectView({
+      el: $('#editLocModal #loc_subaci'),
+      collection: [],
+    }).render();
+    selectSubacias.listenTo(exploracao, 'change:loc_bacia', function(model, value, options){
+      this.update(subacias.where({'parent': model.get('loc_bacia')}));
+    });
+
+    new Backbone.UILib.WidgetsView({
+      el: $('#editLocModal'),
+      model: exploracao
+    }).render();
+
+  },
+
   remove: function () {
     Backbone.View.prototype.remove.call(this);
-    _.invoke(this.subviews, 'remove');
+    _.invoke(this.subViews, 'remove');
   }
 
 });
