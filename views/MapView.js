1f2565b9a58c23f6aaeea0d91a85e3104278ef31
Francisco Puga <fpuga@icarto.es>
2016-04-06 19:21:01 +0200

    Adds a very basic pop up with a link to the exploracao

--- views/MapView.js
+++ views/MapView.js
@@ -6,32 +6,50 @@ Backbone.SIXHIARA.MapView = Backbone.View.extend({
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     });
 
-    this.geoJSONLayer = L.geoJson(this.collection.toGeoJSON());
-    this.map = L.map(this.el.id, {
-      center: [-13, 39.25],
-      zoom: 8,
-      layers: [base, this.geoJSONLayer]
+    var southWest = L.latLng(-23, 31),
+    northEast = L.latLng(-9, 43),
+    maxBounds = L.latLngBounds(southWest, northEast);
+
+    var self = this;
+    this.geoJSONLayer = L.geoJson(this.collection.toGeoJSON(), {
+      onEachFeature: function(feature, layer) {
+        if (feature.properties) {
+          var exp_id = feature.properties.exp_id;
+          var exp = self.collection.filter({'exp_id': exp_id})[0];
+          layer.bindPopup('<a href="' + exp.urlShow() + '">' + exp_id + '</a>');
+        };
+      }
     });
 
-    if(this.collection.length > 0){
-      this.map.fitBounds(this.geoJSONLayer.getBounds())
-      .setMaxBounds(this.geoJSONLayer.getBounds().pad(0.5));
-    } else{
-      // TODO: zoom to the northen area of Mozambique
-      this.map.fitBounds([[-13, 39.25]]);
-    }
+    this.map = L.map(this.el.id, {
+      layers: [base, this.geoJSONLayer],
+      maxBounds: maxBounds,
+      minZoom: 6,
+    });
   },
 
   update: function(newCollection){
     this.collection = newCollection;
+    this.updateLayer();
+    this.updateMapView();
+  },
+
+  updateLayer: function() {
     this.geoJSONLayer.clearLayers();
     var geojson = this.collection.toGeoJSON();
     if(geojson.features.length > 0){
       this.geoJSONLayer.addData(geojson);
-      this.map.fitBounds(this.geoJSONLayer.getBounds())
-      .setMaxBounds(this.geoJSONLayer.getBounds().pad(0.5));
+    }
+  },
+
+  updateMapView: function() {
+    if(this.geoJSONLayer.getLayers().length > 0){
+      var bounds = this.geoJSONLayer.getBounds();
+      this.map.fitBounds(bounds.pad(0.1))
+      var center = this.map.getCenter();
+      var zoom = this.map.getZoom();
+      if (zoom > 15) this.map.setZoomAround(center, 15);
     } else{
-      // TODO: zoom to the northen area of Mozambique
       this.map.fitBounds([[-13, 39.25]]);
       this.map.setZoom(8);
     }
