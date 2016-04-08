84e4d779160b0f0ea5288c603b1a97e505c62d20
Francisco Puga <fpuga@icarto.es>
2016-04-08 10:01:33 +0200

    Refresh button is always enabled
    It's not enough to listen to 'change' in exploracao. licencia,
    fountains,... emit their own 'change' events that should also be
    listened

--- views/show/ButtonRefreshView.js
+++ views/show/ButtonRefreshView.js
@@ -5,20 +5,11 @@ Backbone.SIXHIARA.ButtonRefreshView = Backbone.View.extend({
     "click": "doClick"
   },
 
-  initialize: function() {
-    this.listenTo(this.model, 'change', this.enableButton);
-    this.$el.prop('disabled', true);
-  },
-
   doClick: function(){
     var refreshConfirmation = confirm('Se ele aceita perder alterações');
     if (refreshConfirmation) {
       window.location = this.model.urlShow();
     }
-  },
-
-  enableButton: function() {
-    this.$el.prop('disabled', false);
   },
 
 });
