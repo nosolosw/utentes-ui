8f31773ff3510492b5e1ea775d02ca96688de868
Francisco Puga <fpuga@icarto.es>
2016-04-26 15:31:09 +0200

    Adds geojson data for the offline map

---
+++ offline/data/json_PaisesPunto.js
@@ -0,0 +1,1 @@
+{"type":"FeatureCollection","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Nome":"TANZANIA"},"geometry":{"type":"Point","coordinates":[34.798104,-6.246482]}},{"type":"Feature","properties":{"Nome":"MALAWI"},"geometry":{"type":"Point","coordinates":[34.295515,-13.201487]}}]}
