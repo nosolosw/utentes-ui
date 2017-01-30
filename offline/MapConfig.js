94cce0f4aca98b4424ecfd2eadb95d4f740d5744
Francisco Puga <fpuga@icarto.es>
2017-01-30 21:51:06 +0100

    Removes a couple of base maps

--- offline/MapConfig.js
+++ offline/MapConfig.js
@@ -32,21 +32,12 @@ Backbone.SIXHIARA.mapConfig = function(mapId, initOptions) {
 
   if (options.online || true) {
     var mapBoxApi = window.localStorage.getItem('mapBoxApi');
-    var thunderforestApi = window.localStorage.getItem('thunderforestApi');
 
     var hotLayer = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
-    var thunderforestLayer = L.tileLayer('https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=' + thunderforestApi, {
-      attribution: 'Maps &copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-
-    var mapboxLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=' + mapBoxApi, {
-      attribution:'© <a href= "https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
-    });
-
-    var iCartoLayer = L.tileLayer('https://api.mapbox.com/styles/v1/fpuga/ciyeq8j4m00362slesun9tw70/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZnB1Z2EiLCJhIjoiRTNkN1h1OCJ9.jfJA6rSdkFVm_AKa3w4vRA', {
+    var iCartoLayer = L.tileLayer('https://api.mapbox.com/styles/v1/fpuga/ciyeq8j4m00362slesun9tw70/tiles/256/{z}/{x}/{y}?access_token=' + mapBoxApi, {
       attribution:'© <a href= "http://icarto.es">iCarto</a>, © <a href= "https://www.mapbox.com/map-feedback/">Mapbox</a>, © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     })
 
@@ -58,12 +49,6 @@ Backbone.SIXHIARA.mapConfig = function(mapId, initOptions) {
   } else if (baseMap === 'HOT') {
     hotLayer.addTo(map);
     offBaseLayer.loadOffline(false);
-  } else if (baseMap === 'Thunderforest') {
-    thunderforestLayer.addTo(map);
-    offBaseLayer.loadOffline(false);
-  } else if (baseMap === 'Mapbox') {
-    mapboxLayer.addTo(map);
-    offBaseLayer.loadOffline(false);
   } else if (baseMap === 'iCarto') {
     iCartoLayer.addTo(map);
     offBaseLayer.loadOffline(false);
@@ -73,8 +58,6 @@ Backbone.SIXHIARA.mapConfig = function(mapId, initOptions) {
   var control = L.control.layers({}, {}, {position:'topleft'}).addTo(map);
   control.addBaseLayer(offBaseLayer, "Offline");
   control.addBaseLayer(hotLayer, "HOT");
-  control.addBaseLayer(thunderforestLayer, "Thunderforest");
-  control.addBaseLayer(mapboxLayer, "Mapbox");
   control.addBaseLayer(iCartoLayer, "iCarto");
 
 
